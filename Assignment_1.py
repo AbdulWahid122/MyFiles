d55a7b95c0bacf22790c175915f2303cea1a0f4f
AbdulWahid122 <109365602+AbdulWahid122@users.noreply.github.com>
2022-07-15 21:19:28 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,85 @@
+""" Here I have just shared the answers with answer number, please check.
+
+******************
+
+Ans.1)  
+
+Values: -87.8, 6, 'hello'
+ 
+Operators: *,-,/,+
+
+**************************
+
+Ans2)
+
+String is a sequence of characters enclosed with single, double, triple quote.
+Variable is a tempory storage where data or value stored.add()
+
+*****************************
+
+Ans3)
+
+Datatypes:
+    
+Datatypes are integer,string, float, boolean, 
+
+*********************
+
+Ans4)
+
+Expressions are nothing but an operator, Different types of operators are,
+Arithmetic Operators:+,-,*,/
+Relational Operators:>,<,==,!=
+Logical Operators: &, |
+
+****************************
+
+Ans 5)
+
+Expression is an operator as we have discussed and Statement is anything written in python.
+
+*******************************
+
+
+Ans 6)
+ 
+bacon=22
+# print(bacon+1)
+
+Ans:23 
+
+***************************
+
+Ans 7)
+
+a="spam"
+b="spamspam"
+# print(a+b)
+
+Ans: "spamspamspam"
+
+print(a*3)
+
+# Ans:"spamspamspam"
+
+*********************************
+
+
+Ans8)
+Because variable name can not be start with a digit.
+
+*********************************
+
+Ans9)
+
+int(),float(),str()
+
+************************
+Ans10)
+
+Python cannot concatenate a string and integer.
+
+"""
+
+
+
