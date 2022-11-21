4fb61c7aadb4fab1ad8b7836e85ae6c0960b8637
AbdulWahid122 <109365602+AbdulWahid122@users.noreply.github.com>
2022-11-21 01:03:18 -0800

    Add files via upload

---
+++ assignment9.py
@@ -0,0 +1,45 @@
+"""
+1.To what does a relative path refer?
+Relative paths are relative to the current working directory.
+
+
+2. What does an absolute path start with your operating system?
+Absolute path start with the root folder, such as / or C:\.
+
+
+3.What do the functions os.getcwd() and os.chdir() do?
+
+The os.getcwd() function returns the current working directory. The os.chdir() function changes
+the current working directory.
+
+
+4.What are the . and .. folders?
+The . folder is the current folder and .. is the parent folder.
+
+
+5.In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+C:\bacon\eggs is directory and spam.txt is the base name.
+
+
+6.What are the three "mode" arguments that can be passed to the open() function?
+The string 'r' for read mode, 'w' for write mode and 'a' for append mode.
+
+7. what happens if an existing file is opened in write mode?
+An existing file opened in the write mode is erase and completely overwritten.
+
+
+8.How do you tell the difference between read() and readlines()?
+The read() method returns the file's entire content as a single string value. The readlines()
+method returns a list of string, where each string is a line from the file's contents.
+
+9.What data structure does a shelf value resemble?
+A shelf value resembles a dictionary value, it is keys and values.
+
+
+
+
+
+
+
+
+"""
